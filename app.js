458b7f6d42e049689c2af6c93f030c4b7e659a54
Pille Ulmas <pulmas@klass2-12-l.intra.itcollege.ee>
2017-02-11 16:17:30 +0200

    Esimene commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var main = function() {
+ 
+ $(document).load(function () {
+$('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+});
+ 
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+
+
+};
+$(document).ready(main);
